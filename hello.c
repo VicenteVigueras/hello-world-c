29833c786f2aed63f9c6dd7a6993d2a1a3394e0d
VicenteVigueras <vicente.viguerassilva@gmail.com>
2026-01-29 22:03:56 -0500

    chore: commented output

--- hello.c
+++ hello.c
@@ -4,5 +4,7 @@ int main() {
     printf("Hello, World!\n");
     int myInt = 10;
     printf("Size of int: %zu bytes\n", sizeof(myInt));
+    // Hello, World!
+    // Size of int: 4 bytes
     return 0;
 }
