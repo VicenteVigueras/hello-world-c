a766f1fe8cf0353d2cb53e8cbc7767686f164443
VicenteVigueras <vicente.viguerassilva@gmail.com>
2026-01-29 22:06:59 -0500

    feat: expand to different types

--- hello.c
+++ hello.c
@@ -6,5 +6,15 @@ int main() {
     printf("Size of int: %zu bytes\n", sizeof(myInt));
     // Hello, World!
     // Size of int: 4 bytes
+
+    float myFloat = 10.1;
+    printf("Size of float: %zu bytes\n", sizeof(myFloat));
+
+    double myDouble = 100.10;
+    printf("Size of double: %zu bytes\n", sizeof(myDouble));
+
+    char myChar = 'A';
+    printf("Size of char: %zu bytes\n", sizeof(myChar));
+
     return 0;
 }
