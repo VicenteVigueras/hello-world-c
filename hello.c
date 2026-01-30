71e61e9ead1bc1758a216e4417684f062a1c3bcb
VicenteVigueras <vicente.viguerassilva@gmail.com>
2026-01-29 22:12:22 -0500

    feat: char array to represent a string and how memory is stored

--- hello.c
+++ hello.c
@@ -4,8 +4,6 @@ int main() {
     printf("Hello, World!\n");
     int myInt = 10;
     printf("Size of int: %zu bytes\n", sizeof(myInt));
-    // Hello, World!
-    // Size of int: 4 bytes
 
     float myFloat = 10.1;
     printf("Size of float: %zu bytes\n", sizeof(myFloat));
@@ -15,6 +13,22 @@ int main() {
 
     char myChar = 'A';
     printf("Size of char: %zu bytes\n", sizeof(myChar));
+
+    char myWord[] = "Example sentence";
+    printf("Size of Word: %zu bytes\n", sizeof(myWord));
+
+    /*
+    ---------
+    Output:
+    ---------
+    Hello, World!
+    Size of int: 4 bytes
+    Size of float: 4 bytes
+    Size of double: 8 bytes
+    Size of char: 1 bytes
+    Size of Word: 17 bytes
+    ----------
+    */
 
     return 0;
 }
