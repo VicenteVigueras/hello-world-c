804dfcd4c161b4765257f40ce9c4df92fafe8764
VicenteVigueras <vicente.viguerassilva@gmail.com>
2026-02-16 12:38:37 -0500

    fix: remove break statements and inefficient variable initializations

--- calculator/Calculator.java
+++ calculator/Calculator.java
@@ -24,10 +24,7 @@ public class Calculator {
         Scanner scan = new Scanner(System.in);
         Set<Integer> functions = new HashSet<>(Set.of(1,2,3,4));
         
-        int input = -1;
-        int a = -1;
-        int b = -1;
-        while(input != 0) {
+        while(true) {
             System.out.println("====================");
             System.out.println("1. Add");
             System.out.println("2. Subtract");
@@ -35,27 +32,26 @@ public class Calculator {
             System.out.println("4. Divide");
             System.out.println("0. Exit");
             System.out.print(">> ");
-            input = scan.nextInt();
+            int input = scan.nextInt();
+
+            if(input == 0) break;
+            
             if(functions.contains(input)) {
                 System.out.print(">> Select A: ");
-                a = scan.nextInt();
+                int a = scan.nextInt();
                 System.out.print(">> Select B: ");
-                b = scan.nextInt();
+                int b = scan.nextInt();
                 if(input == 1) {
                     System.out.println("The sum is: " + calculator.add(a,b));
-                    break;
                 }
                 if(input == 2) {
                     System.out.println("The difference is: " + calculator.subtract(a,b));
-                    break;
                 }
                 if(input == 3) {
                     System.out.println("The product is: " + calculator.multiply(a,b));
-                    break;
                 }
                 if(input == 4) {
                     System.out.println("The quotient is: " + calculator.divide(a,b));
-                    break;
                 }
             }
         }
