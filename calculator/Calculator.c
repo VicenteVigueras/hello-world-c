532130d2d84d4fce1391281e95e4d9cd01766b9c
VicenteVigueras <vicente.viguerassilva@gmail.com>
2026-02-15 22:41:38 -0500

    feat: subtract, multiply, and divide in calculator

--- calculator/Calculator.c
+++ calculator/Calculator.c
@@ -1,13 +1,34 @@
 #include <stdio.h>
 
-int add_numbers(int a, int b) {
+int add(int a, int b) {
     int result = a + b;
     return result; 
 }
 
+int subtract(int a, int b) {
+    int result = a - b;
+    return result; 
+}
+
+int multiply(int a, int b) {
+    int result = a * b;
+    return result; 
+}
+
+int divide(int a, int b) {
+    int result = a / b;
+    return result; 
+}
+
 int main() {
-    int sum = add_numbers(1,2);
+    int sum = add(1,2);
+    int difference = subtract(1,2);
+    int product = multiply(1,2);
+    int quotient = divide(1,2);
     printf("The sum is: %d\n", sum);
+    printf("The difference is: %d\n", difference);
+    printf("The product is: %d\n", product);
+    printf("The quotient is: %d\n", quotient);
     return 0;
 }
 
--- calculator/Calculator.java
+++ calculator/Calculator.java
@@ -1,9 +1,25 @@
 public class Calculator {
-    public static int add(int a, int b) {
+
+    public int add(int a, int b) {
         return a + b;
     }
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        return a / b;
+    }
+
     public static void main(String[] args) {
-        System.out.println(add(1,2));
-        System.out.println(add(2,3));
+        Calculator calculator = new Calculator();
+        System.out.println("The sum is: " + calculator.add(1,2));
+        System.out.println("The difference is: " + calculator.subtract(1,2));
+        System.out.println("The multiply is: " + calculator.multiply(1,2));
+        System.out.println("The divide is: " + calculator.divide(1,2));
     }
 }
